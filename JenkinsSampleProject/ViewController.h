966bc47fcaa8161269ff886e057e96bf6f4d1f4f
DHURIU <umesh_dhuri@whirlpool.com>
2016-04-07 18:36:09 +0530

    Source Code
    Source Code

---
+++ JenkinsSampleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsSampleProject
+//
+//  Created by Jenkins on 07/04/16.
+//  Copyright © 2016 Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JenkinsSampleProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JenkinsSampleProject
+//
+//  Created by Jenkins on 07/04/16.
+//  Copyright © 2016 Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
