ba91919d14591d7e4145c1aecfff5fe4a9b8fb87
DHURIU <umesh_dhuri@whirlpool.com>
2016-04-13 16:26:23 +0530

    Added New Class
    Added New Class

--- JenkinsSampleProject/AppDelegate.m
+++ JenkinsSampleProject/AppDelegate.m
@@ -19,7 +19,7 @@
     // Override point for customization after application launch.
     return YES;
     
-    AppDelegate *appdelegate = [[UIApplication sharedApplication] delegate] ;
+    
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
---
+++ JenkinsSampleProject/DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  JenkinsSampleProject
+//
+//  Created by Jenkins on 13/04/16.
+//  Copyright © 2016 Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@end
